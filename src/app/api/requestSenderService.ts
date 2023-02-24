15c41d6c46e386532ed75fb1216c80b56d55481f
Anton Pronin <gorka19800@gmail.com>
2023-02-24 15:36:31 +0300

    Started adding classes for http messaging with backend

---
+++ src/app/api/requestSenderService.ts
@@ -0,0 +1,45 @@
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RequestSenderService {
+  constructor(private _httpClient: HttpClient){
+
+  }
+
+  sendGetRequest<T>(url: string, params: HttpParams): Observable<T> {
+    const options = {
+      params: params
+    };
+
+    return this._httpClient.get<T>(url, options);
+  }
+
+  sendPostRequest<T>(url: string, body: T, params: HttpParams): Observable<T> {
+    const options = {
+      params: params
+    };
+
+    return this._httpClient.post<T>(url, body, options);
+  }
+
+  sendPutRequest<T>(url: string, body: T, params: HttpParams): Observable<T> {
+    const options = {
+      params: params
+    };
+
+    return this._httpClient.put<T>(url, body, options);
+  }
+
+  sendDeleteRequest<T>(url: string, body: T, params: HttpParams): Observable<T> {
+    const options = {
+      params: params,
+      body: body
+    };
+
+    return this._httpClient.delete<T>(url, options);
+  }
+}
